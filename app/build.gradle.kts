f878d0c9d13d0548ad204251fd00ff08a3174d8f
Xusa <xusa@iesdoctorbalmis.com>
2024-03-25 23:34:17 +0100

    error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // implementation("com.github.pmdmiesbalmis:utilities:1.0")
-   // implementation(project(":utilities"))
+    ///implementation("com.github.pmdmiesbalmis:components:1.0")
+   // implementation(project(":components"))
 }
 
 
--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -51,4 +52,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.2")}
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.2")
+
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.pmdmiesbalmis"
+            artifactId = "components"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
